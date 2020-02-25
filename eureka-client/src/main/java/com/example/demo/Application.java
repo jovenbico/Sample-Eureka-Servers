50dcbb05e7a5cab5dd544049c001870840c06d24
BICO Joven Frankie <JovenFrankie.Bico@champ.aero>
2020-02-25 18:16:52 +0800

    add application.java files

---
+++ eureka-client/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world";
+	}
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class)//
+				.web(true)//
+				.run(args);
+	}
+
+}
